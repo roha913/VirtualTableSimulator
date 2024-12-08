a4e13ebcef5f2858dab3c4518e8784d3f941aefd
Rohan Jay <rjay2@jh.edu>
2024-12-07 19:14:29 -0500

    Made a_Class.h --- a class to represent classes(the name and the virtual pointer)

---
+++ a_Class.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "HashMap.h"
+
+#ifndef a_Class_H
+#define a_Class_H
+class a_Class{
+        public:
+                std::string name;
+                HashMap vpointer;
+}
+
+#endif a_Class_H
