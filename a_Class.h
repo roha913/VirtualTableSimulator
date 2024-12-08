1efb54503e4c65ef63e8b5fff10cf95c124381ef
Rohan Jay <rjay2@jh.edu>
2024-12-07 20:17:54 -0500

    Added a vector of a_Class pointers to store the classes that inherit from a_Class

--- a_Class.h
+++ a_Class.h
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "HashMap.h"
 
-#ifndef a_Class_H
-#define a_Class_H
+#ifndef a_CLass_H
+#define a_CLass_H
 class a_Class{
         public:
                 std::string name;
-                HashMap vpointer;
+                HashMap* vpointer;
+		std::vector<a_Class*> derived_classes;
+		~a_Class(){
+			 delete vpointer;
+			 for(std::vector<a_Class*>::const_iterator it = derived_classes.cbegin(); it != derived_classes.cend(); it++){
+				delete *it;
+			 }
+		}
 }
 
-#endif a_Class_H
+#endif a_CLass_H
